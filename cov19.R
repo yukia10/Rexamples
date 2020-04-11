da95e95e11c36406b9176307fab0a2e270904fb6
Yuki Atoh <yuki.atoh@gmail.com>
2020-04-11 10:18:30 +0900

    remove locale dependency

--- cov19.R
+++ cov19.R
@@ -4,63 +4,63 @@
 #   https://de.wikipedia.org/wiki/COVID-19-Pandemie_in_Deutschland
 #   https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Situationsberichte/Archiv.html
 
-### Scraping
-olocale <- Sys.getlocale("LC_TIME")
-sysname <- Sys.info()["sysname"]
-Sys.setlocale(
-  "LC_TIME",
-  switch(sysname, Linux="de_DE.UTF-8", Darwin="de_DE", Windows="German", "C")) # OSX not checked
+# Federal States of Germany
+state <- c(
+  "BW", "BY", "BE", "BB", "HB", "HH", "HE", "MV",
+  "NI", "NW", "RP", "SL", "SN", "ST", "SH", "TH")
 
+# Former East Germany (GDR), Former West Germany (BRD), Berlin
+ew <- c(
+  "w", "w", "b", "e", "w", "w", "w", "e",
+  "w", "w", "w", "w", "e", "e", "w", "e")
+names(ew) <- state
+
+# Month name in Germany
+month.de <- c(
+  "Jan", "Feb", "M\u00e4r", "Apr", "Mai", "Jun", 
+  "Jul", "Aug", "Sep", "Okt", "Nov", "Dez")
+
+### Scraping
 library(rvest)
-url <- "https://de.wikipedia.org/wiki/COVID-19-Pandemie_in_Deutschland"
-# url <- "COVID-19-Pandemie in Deutschland – Wikipedia.html"
+
+# url <- "https://de.wikipedia.org/wiki/COVID-19-Pandemie_in_Deutschland"
+url <- "COVID-19-Pandemie in Deutschland – Wikipedia.html"
 tbl <- url %>% read_html %>% html_nodes("table")
 
 conv_tbl <- function(tbl) {
   t <- html_table(tbl, dec=".")[[1]]
   t <- t[-nrow(t), 1:17]
-  t <- data.frame(lapply(t, function(x) gsub("[(].*[)]", "", x)), stringsAsFactors=FALSE)
-  t <- data.frame(lapply(t, function(x) gsub("[.]", "", x)), stringsAsFactors=FALSE)
-  t <- data.frame(lapply(t, function(x) gsub("[\u2013\u2014]", "0", x)), stringsAsFactors=FALSE)
-  t[, 1] <- gsub(".*♠", "", t[, 1])
-  t[, 1] <- gsub("\u00a0", " ", t[, 1])
-  if (sysname == "Windows")
-    t[, 1] <- gsub("M\u00e4r", "Mrz", t[, 1])
-  data.frame(
-    lapply(t[, -1], as.numeric),
-    row.names=as.character(strptime(t[, 1], "%d %b %Y")))
+  t <- data.frame(lapply(
+    t, function(x) gsub("[(].*[)]", "", x)), stringsAsFactors=FALSE)
+  t <- data.frame(lapply(
+    t, function(x) gsub("[.]", "", x)), stringsAsFactors=FALSE)
+  t <- data.frame(lapply(
+    t, function(x) gsub("[\u2013\u2014]", "0", x)), stringsAsFactors=FALSE) # dash to 0
+  t[, 1] <- gsub(".*\u2660", "", t[, 1])
+  t[, 1] <- gsub("\u00a0", " ", t[, 1]) # replace space
+  t[, 1] <- sapply(strsplit(t[, 1], " "), function(x) 
+    sprintf("%s-%02d-%s", x[3], match(x[2], month.de), x[1]))
+  data.frame(lapply(t[, -1], as.numeric), row.names=t[, 1])
 }
 
 t1 <- conv_tbl(tbl[4]) # Cum. infections
-t2 <- conv_tbl(tbl[5]) # Cum. infections (per 100,000 inhabitants)
+t2 <- conv_tbl(tbl[5]) # Cum. infections / 100,000 pop.
 t3 <- conv_tbl(tbl[6]) # Cum. deaths
 
-Sys.setlocale("LC_TIME", olocale)
-stopifnot(
-  row.names(t1)[nrow(t1)] == row.names(t2)[nrow(t2)],
-  all(colnames(t1) == colnames(t2)), all(colnames(t1) == colnames(t3)))
+stopifnot(all(colnames(t2) == colnames(t3)), all(colnames(t1) == colnames(t3)))
+
+k <- tail(intersect(row.names(t1), row.names(t2)), 1) # latest common date
+pop <- as.numeric(100000 * t1[k, ] / t2[k, ]) # larger number will make less erroneous
+t4 <- data.frame(t(t(as.matrix(t3)) / pop * 100000)) # Cum. deaths / 100,000 pop.
 
 ### Analysis
 library(growthcurver)
-
-pop <- as.numeric(100000 * t1[nrow(t1), ] / t2[nrow(t2), ]) # larger number will make less erroneous
-t4 <- data.frame(t(t(as.matrix(t3)) / pop * 100000)) # Cum. deaths (per 100,000 inhabitants)
 
 L <- lapply(t4, function(x) SummarizeGrowth(seq_along(rownames(t4)), x, bg_correct="none"))
 t5 <- data.frame(sapply(L, function(x) predict(x$model))) # Fitted
 row.names(t5) <- row.names(t4)
 
 ### Plot
-# Federal States of Germany
-state <- c(
-  "BW", "BY", "BE", "BB", "HB", "HH", "HE", "MV",
-  "NI", "NW", "RP", "SL", "SN", "ST", "SH", "TH")
-
-ew <- c(
-  "w", "w", "b", "e", "w", "w", "w", "e",
-  "w", "w", "w", "w", "e", "e", "w", "e")
-names(ew) <- state
-
 col <-c(e="red", w="blue", b="purple")
 fg <- col[ew]
 names(fg) <- state
@@ -69,7 +69,7 @@ x <- as.POSIXct(row.names(t4))
 matplot(
   x, t4, xaxt="n", type="l", lty=1, col=fg,
   main="COVID-19 deaths in Germany",
-  xlab="Date", ylab="Deaths per 100,000 inhabitants")
+  xlab="Date", ylab="Deaths / 100,000 pop.")
 axis.POSIXct(
   side=1, at=seq(x[1], x[length(x)], "day"), las=2, format="%m/%d")
 text(x[length(x)], t4[nrow(t4), ], names(t4), col=fg, adj=-0.2, cex=0.8)
@@ -105,7 +105,7 @@ ggplot(td, aes(as.Date(date), death)) +
       rownames(t4)[nrow(t4)]),
     caption = "Data Source: Wikipedia based on Robert Koch Institut",
     x = "Date",
-    y = "Deaths per 100,000 inhabitants") +
+    y = "Deaths / 100,000 pop.") +
   geom_text(
     x=-Inf, y=Inf, aes(label=sprintf("%.2f", t4[nrow(t4), state])),
     vjust=1.2, hjust=-0.2, size=5, check_overlap=TRUE) +
